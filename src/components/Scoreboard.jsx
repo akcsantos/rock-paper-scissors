3d7d505b0bce2e50e58f13063ab1ecf00ad93f96
akcsantos <65388001+akcsantos@users.noreply.github.com>
2024-12-12 15:50:16 +0800

    fixed design for scoreboard, to add functionality later

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -3,8 +3,14 @@ import styles from "./scoreboard.module.css";
 export default function Scoreboard() {
   return (
     <div className={styles.scoreboard}>
-      <div>Human score</div>
-      <div>Computer score</div>
+      <div className={styles.playerScoreboard}>
+        <h2 className={styles.playerName}>Human Score</h2>
+        <h2>0</h2>
+      </div>
+      <div className={styles.computerScoreboard}>
+        <h2 className={styles.computerName}>Computer Score</h2>
+        <h2>0</h2>
+      </div>
     </div>
   );
 }

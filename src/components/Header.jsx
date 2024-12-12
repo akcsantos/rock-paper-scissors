675c2fa67b62aeedf5aa39969af8b4e720ee46de
akcsantos <65388001+akcsantos@users.noreply.github.com>
2024-12-12 15:25:40 +0800

    fixed title header for page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import styles from "./header.module.css";
+
 export default function Header() {
-  return <div>Rock Paper Scissors</div>;
+  return <h1 className={styles.title}>Rock Paper Scissors</h1>;
 }
